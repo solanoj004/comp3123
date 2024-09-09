a6202d590f21653073e5fa47791f20fbb76f076a
solanoj004 <95721366+solanoj004@users.noreply.github.com>
2024-09-08 23:08:14 -0400

    Lab Exercise Week 1

---
+++ lab_exercise_week_1/lab_exercise.js
@@ -0,0 +1,29 @@
+function find_angle_type(angle) {
+    if (angle === 0) {
+        return "No angle";
+    }
+    else if (angle < 90) {
+        return "Acute angle";
+    }
+    else if (angle === 90) {
+        return "Right angle";
+    }
+    else if (angle < 180){
+        return "Obtuse angle";
+    }
+    else if (angle === 180) {
+        return "Straight angle";
+    }
+    // When user inputs a string or a number less than 0 or greater than 180
+    else {
+        return "Please input a valid angle. Must be between 0 and 180 degrees";
+    }
+}
+
+console.log(find_angle_type(181));
+console.log(find_angle_type(0));
+console.log(find_angle_type(90));
+console.log(find_angle_type(180));
+console.log(find_angle_type(45));
+console.log(find_angle_type(131));
+console.log(find_angle_type("string test"))
